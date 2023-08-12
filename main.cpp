c049eab96864c848adb022fd7fd6d7b354e1cffc
kjohnbaba <105748315+kjohnbaba@users.noreply.github.com>
2023-08-12 17:44:36 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,22 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    // Write C++ code here
+    std::cout << "Hello world!";
+    
+    int num1;
+    int num2;
+    cout << "Enter num1 " ;
+    cin >> num1;
+    
+    cout << "Enter num2 ";
+    cin >> num2;
+    
+    cout << num1+num2 ;
+    
+    return 0;
+}
